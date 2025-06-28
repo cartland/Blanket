5fbc5ee96c3e870737f6ec589974d849f20e3ff1
cartland <c.cartland@gmail.com>
2025-06-28 12:44:06 -0700

    Ktlint on Main.kt files

--- composeApp/src/desktopMain/kotlin/com/chriscartland/blanket/Main.kt
+++ composeApp/src/desktopMain/kotlin/com/chriscartland/blanket/Main.kt
@@ -3,11 +3,12 @@ package com.chriscartland.blanket
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "Blanket",
-    ) {
-        App()
+fun main() =
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "Blanket",
+        ) {
+            App()
+        }
     }
-}
