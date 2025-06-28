251d2a243c145277e2c862f75304fca7ae83d97b
cartland <c.cartland@gmail.com>
2025-06-28 13:33:59 -0700

    Use Theme colors and style

--- composeApp/src/commonMain/kotlin/com/chriscartland/blanket/App.kt
+++ composeApp/src/commonMain/kotlin/com/chriscartland/blanket/App.kt
@@ -6,11 +6,14 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -32,12 +35,19 @@ fun App() {
         var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
+                .background(MaterialTheme.colorScheme.background)
                 .safeContentPadding()
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+            Button(
+                onClick = { showContent = !showContent },
+                colors = ButtonDefaults.buttonColors(),
+            ) {
+                Text(
+                    "Click me!",
+                    style = MaterialTheme.typography.labelLarge,
+                )
             }
             AnimatedVisibility(
                 showContent,
@@ -47,7 +57,11 @@ fun App() {
                 val greeting = remember { Greeting().greet() }
                 Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
                     Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+                    Text(
+                        "Compose: $greeting",
+                        style = MaterialTheme.typography.labelLarge,
+                        color = MaterialTheme.colorScheme.onBackground,
+                    )
                 }
             }
         }
