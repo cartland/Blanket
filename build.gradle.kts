6be8808144d6f638af963feb0e2aa49ec4cb3350
cartland <c.cartland@gmail.com>
2025-06-29 00:01:02 -0700

    Add reference module

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.spotless) apply false
+    alias(libs.plugins.androidKotlinMultiplatformLibrary) apply false
 }
 
 subprojects {
---
+++ shared-reference/src/androidMain/kotlin/com/example/shared_reference/Platform.android.kt
@@ -0,0 +1,3 @@
+package com.example.shared_reference
+
+actual fun platform() = "Android"
